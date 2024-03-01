ce031242d920043c6fcc7aa9976520e585f7ab62
Francis <francis.descoteaux.henault@gmail.com>
2024-03-01 17:54:19 -0500

    Added ineheritance base scripts
    + GameInstance
    + GameInstanceSubSystem

---
+++ Source/CharlesInTown/CITGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CITGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARLESINTOWN_API UCITGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
