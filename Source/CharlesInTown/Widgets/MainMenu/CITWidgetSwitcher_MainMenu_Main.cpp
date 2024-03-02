6a36a6be6d9fbc1f42c71070c280506ebea05773
Francis <francis.descoteaux.henault@gmail.com>
2024-03-02 01:42:46 -0500

    MainMenu
    + Added WidgetSwitcher custom c++ script
    + Added Widget Blueprint for MainMenu main panel
    + Added WidgetSwitcher Blueprint for MainMenu_Main

---
+++ Source/CharlesInTown/Widgets/MainMenu/CITWidgetSwitcher_MainMenu_Main.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CITWidgetSwitcher_MainMenu_Main.h"
+
---
+++ Source/CharlesInTown/Widgets/MainMenu/CITWidgetSwitcher_MainMenu_Main.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharlesInTown/Widgets/CITWidgetSwitcherBase.h"
+#include "CITWidgetSwitcher_MainMenu_Main.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARLESINTOWN_API UCITWidgetSwitcher_MainMenu_Main : public UCITWidgetSwitcherBase
+{
+	GENERATED_BODY()
+	
+};
