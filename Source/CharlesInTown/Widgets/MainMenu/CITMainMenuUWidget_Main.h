39f166e6a5ec2d6a9101b627080847033a2ff6c2
Francis <francis.descoteaux.henault@gmail.com>
2024-03-02 01:14:25 -0500

    MainMenu widgets
    * Added UMG dependency in uproject file
    + Added UserWidget child base script (Abstract) + child MainMenu widget for main panel/1rst screen

---
+++ Source/CharlesInTown/Widgets/MainMenu/CITMainMenuUWidget_Main.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharlesInTown/Widgets/CITUserWidgetBase.h"
+#include "CITMainMenuUWidget_Main.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARLESINTOWN_API UCITMainMenuUWidget_Main : public UCITUserWidgetBase
+{
+	GENERATED_BODY()
+	
+};
