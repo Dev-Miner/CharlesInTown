95a9efc95cfa6a2b24eb17cc94cfd89ee0ae1133
Francis <francis.descoteaux.henault@gmail.com>
2024-03-02 01:31:33 -0500

    MainMenu and Core
    * Moved GameInstance cpp scripts for organisation
    + Added GameMode child script to be used as base
    + Added Some MainMenu source files (Default pawn, GameMode, PlayerController)

---
+++ Source/CharlesInTown/Core/CITGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "CITGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARLESINTOWN_API ACITGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CharlesInTown/Menus/MainMenu/CITGameMode_MainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharlesInTown/Core/CITGameMode.h"
+#include "CITGameMode_MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARLESINTOWN_API ACITGameMode_MainMenu : public ACITGameMode
+{
+	GENERATED_BODY()
+	
+};
